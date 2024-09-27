d8a924d670b45cba80a284f9eaa4157353cf071e
IT24102763 <it24102763@my.sliit.lk>
2024-09-27 09:06:54 +0530

    ADD IT24102763Lab6Q2A.java to Q2A

---
+++ Q2/IT24102763Lab6Q2A.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class IT24102763Lab6Q2A{
+	public static void main(String []args){
+		
+		int Count = 0;
+		
+		while(Count<10){
+			Count = Count + 1;
+		System.out.print(" "+Count+" ");
+		}
+		
+	}
+}
